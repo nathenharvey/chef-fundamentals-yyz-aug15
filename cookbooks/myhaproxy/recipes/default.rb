1fb6504f4d93d5448b8745e765b625d85dfcd926
Nathen Harvey <nharvey@chef.io>
2015-08-06 13:15:01 -0400

    Search for web nodes

--- cookbooks/myhaproxy/recipes/default.rb
+++ cookbooks/myhaproxy/recipes/default.rb
@@ -4,18 +4,20 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-node.default['haproxy']['members'] = [
-  {
-    'hostname' => 'ec2-54-172-190-60.compute-1.amazonaws.com',
-    'ipaddress' => '54.172.190.60',
-    'port' => 80,
-    'ssl_port' => 80
-  },{
-    'hostname' => 'ec2-52-4-58-39.compute-1.amazonaws.com',
-    'ipaddress' => '52.4.58.39',
-    'port' => 80,
-    'ssl_port' => 80
-  }
-]
+all_web_nodes = search("node","role:web")
 
-include_recipe 'haproxy::default'
+members = []
+
+all_web_nodes.each do |web_node|
+  member = {
+    "hostname" => web_node["cloud"]["public_hostname"],
+    "ipaddress" => web_node["cloud"]["public_ipv4"],
+    "port" => 80,
+    "ssl_port" => 80
+  }
+  members.push(member)
+end
+
+node.default['haproxy']['members'] = members
+
+include_recipe "haproxy::default"
