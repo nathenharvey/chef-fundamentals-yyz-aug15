74510128a0cf6fdaced7bb1423df1b96b14d7e86
Nathen Harvey <nharvey@chef.io>
2015-08-06 12:07:09 -0400

    Add node3 to the load balancer

--- cookbooks/myhaproxy/recipes/default.rb
+++ cookbooks/myhaproxy/recipes/default.rb
@@ -4,11 +4,18 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-node.default['haproxy']['members'] = [{
+node.default['haproxy']['members'] = [
+  {
     'hostname' => 'ec2-54-172-190-60.compute-1.amazonaws.com',
     'ipaddress' => '54.172.190.60',
     'port' => 80,
     'ssl_port' => 80
-}]
+  },{
+    'hostname' => 'ec2-52-4-58-39.compute-1.amazonaws.com',
+    'ipaddress' => '52.4.58.39',
+    'port' => 80,
+    'ssl_port' => 80
+  }
+]
 
 include_recipe 'haproxy::default'
