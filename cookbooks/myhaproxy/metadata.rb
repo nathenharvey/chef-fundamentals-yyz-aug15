861f57087e5c4d725863c157b4e964fc7498ed3d
Nathen Harvey <nharvey@chef.io>
2015-08-06 13:49:23 -0400

    bump myhaproxy

--- cookbooks/myhaproxy/metadata.rb
+++ cookbooks/myhaproxy/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures myhaproxy'
 long_description 'Installs/Configures myhaproxy'
-version '0.3.1'
+version '0.3.2'
 
 depends 'haproxy', '= 1.6.6'
